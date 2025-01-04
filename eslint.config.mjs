98733ee10748dd1a32e63d1ed556d9a7b0adbb2a
Oleh Kuper <ok1804@ukr.net>
2025-01-04 16:01:26 +0200

    feat: setup eslint-config prettier, variables, fonts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,53 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ),
+  {
+    rules: {
+      'react/jsx-filename-extension': [
+        2,
+        { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      ],
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-no-constructed-context-values': 'off',
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
+      'import/prefer-default-export': 'off',
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'arrow-body-style': 'off',
+      'no-underscore-dangle': 'off',
+      'no-use-before-define': 'off',
+      'no-confusing-arrow': 'off',
+      'no-restricted-syntax': 'off',
+      'no-await-in-loop': 'off',
+      camelcase: 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,80 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+          hover: 'hsl(var(--accent-hover))',
+          secondary: 'hsl(var(--accent-secondary))',
+          'hover-secondary': 'hsl(var(--accent-hover-secondary))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+          hover: 'hsl(var(--destructive-hover))',
+        },
+        border: 'hsl(var(--border))',
+        input: {
+          DEFAULT: 'hsl(var(--input))',
+          foreground: 'hsl(var(--input-foreground))',
+        },
+        ring: 'hsl(var(--ring))',
+        'check-box-foreground': 'hsl(var(--check-box-foreground))',
+        placeholder: 'hsl(var(--placeholder))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      fontSize: {
+        'heading-xl': ['24px', { lineHeight: '30px', fontWeight: '700' }],
+        'heading-l': ['18px', { lineHeight: '23px', fontWeight: '700' }],
+        'heading-m': ['15px', { lineHeight: '19px', fontWeight: '700' }],
+        'heading-s': ['12px', { lineHeight: '15px', fontWeight: '700', letterSpacing: '2.4px' }],
+        'body-l': ['13px', { lineHeight: '23px', fontWeight: '500' }],
+        'body-m': ['12px', { lineHeight: '15px', fontWeight: '700' }],
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line global-require, @typescript-eslint/no-require-imports
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
