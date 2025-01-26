329cc4193b60a528490a2f3091788d648e59fd19
Oleh Kuper <ok1804@ukr.net>
2025-01-26 22:15:03 +0200

    fix: fix bug with changing theme

--- src/components/molecules/ToggleThemeButton.tsx
+++ src/components/molecules/ToggleThemeButton.tsx
@@ -6,13 +6,15 @@ import { useTheme } from 'next-themes';
 import { Switch } from '../atoms/switch';
 
 const ToggleThemeButton = () => {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+  const isDarkTheme = theme === 'dark';
 
   return (
     <div className="mx-auto flex h-12 w-[235px] items-center justify-center space-x-6 rounded-md bg-primary lg:w-[251px]">
       <Sun className="h-4 w-4 text-secondary-foreground" />
       <Switch
-        onCheckedChange={(isChecked) => setTheme(isChecked ? 'dark' : 'light')}
+        checked={isDarkTheme}
+        onCheckedChange={() => setTheme(isDarkTheme ? 'light' : 'dark')}
       />
       <MoonStar className="h-4 w-4 text-secondary-foreground" />
     </div>
