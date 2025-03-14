358ba4d2a53d1d6c1f9a4a93d496ae68dd0426d4
Oleh Kuper <ok1804@ukr.net>
2025-03-14 19:02:48 +0200

    feat: update subtask status

--- src/components/molecules/Subtask.tsx
+++ src/components/molecules/Subtask.tsx
@@ -1,17 +1,41 @@
+import { useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 
+import useSubtaskMutation from '@/hooks/subtasks/use-subtask-mutation';
 import { cn } from '@/lib/utils';
+import { showNotification } from '@/utils';
 
 import { Checkbox } from '../atoms/checkbox';
 
 type Props = {
+  id: number;
   title: string;
   isCompleted: boolean;
 };
 
-const Subtask = ({ isCompleted, title }: Props) => {
+const Subtask = ({ id, isCompleted, title }: Props) => {
+  const queryClient = useQueryClient();
+  const { subtaskMutation } = useSubtaskMutation();
+  const { mutate, isPending } = subtaskMutation.update;
+
+  const handleChangeStatus = async () => {
+    mutate(
+      { subtaskId: id, isCompleted: !isCompleted },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ['columns'] });
+          showNotification('success', 'Subtask was updated');
+        },
+      },
+    );
+  };
+
   return (
-    <li className="flex cursor-pointer items-center space-x-[16px] rounded-sm bg-primary p-3 hover:bg-accent-hover-secondary">
+    <li
+      aria-hidden
+      onClick={isPending ? undefined : handleChangeStatus}
+      className="flex cursor-pointer items-center space-x-[16px] rounded-sm bg-primary p-3 hover:bg-accent-hover-secondary"
+    >
       <Checkbox checked={isCompleted} />
       <h3
         className={cn(
